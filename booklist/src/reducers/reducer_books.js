d1d09f87bd9ca142332f41937bd2a49a226f3596
Raf <rafaelmarianorocha@gmail.com>
2018-12-11 23:13:11 -0800

    This commit adds pages to book titles. Simple practice.

--- booklist/src/reducers/reducer_books.js
+++ booklist/src/reducers/reducer_books.js
@@ -1,9 +1,9 @@
 export default function () {
   return [
   // eslint-disable-next-line
-    { title: 'Javascript: The Good Parts' },
-    { title: 'Harry Potter' },
-    { title: 'The Dark Tower' },
-    { title: 'Eloquent Ruby' },
+    { title: 'Javascript: The Good Parts', pages: 39 },
+    { title: 'Harry Potter', pages: 3302 },
+    { title: 'The Dark Tower', pages: 72 },
+    { title: 'Eloquent Ruby', pages: 25 },
   ]
 }
